7741707fbf29c963b6198c1071608039472f85a8
Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>
2026-02-01 22:07:24 +0900

    Create prime_check.py
    Signed-off-by: Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>

---
+++ prime_check.py
@@ -0,0 +1,18 @@
+import math
+def cannot(x):
+    if x == 1: 
+        return False
+    elif x == 2:
+        return True
+        
+    elif x % 2 ==0:
+        return False
+        
+    numbers = list(range(3,int(math.sqrt(x))+1,2))
+    for i in numbers:
+        if x%i == 0:
+            return False
+    return True
+
+number = cannot(44)    
+print(number)
