9ee4a924420ac76f7cb5bb20a993bf2800b21bca
Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>
2026-02-10 22:49:02 +0900

    Update prime_check.py
    Signed-off-by: Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>

--- prime_check.py
+++ prime_check.py
@@ -1,8 +1,8 @@
-def isPrime(x):
+def is_Prime(x):
   if x == 2:
     return True
   elif x <= 1 or x%2 == 0:
-    return Falsei 
+    return False
   i = 3
   while i*i <= x:
     if x%i == 0:
@@ -10,5 +10,14 @@ def isPrime(x):
     i += 2
     return True
     
-num = isPrime(int(input()))  
-print(num)
+user_input = input("整数を入力して下さい　").strip()
+if not user_input:
+  print("何も入力されていません")
+elif user_input.isdigit():
+  num = is_Prime(int(user_input)) 
+  if num == True:
+    print(f"結果:「{user_input}」は素数です")
+  elif num == False: 
+    print(f"結果:「{user_input}」は素数ではありません") 
+else:
+  print(f"「{user_input}」は整数ではありません（文字や記号が含まれています）")
