4fd0b5abc8ae9e57aebcc16907ae44508a530c0a
Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>
2026-02-10 18:26:08 +0900

    Update prime_check.py
    Signed-off-by: Taro-Nakamura-cct <joyamainkyo0624@docomo.ne.jp>

--- prime_check.py
+++ prime_check.py
@@ -1,16 +1,13 @@
 def isPrime(x):
-    if x <= 1:
+  if x == 2:
+    return True
+  elif x <= 1 or x%2 == 0:
+    return Falsei 
+  i = 3
+  while i*i <= x:
+    if x%i == 0:
       return False
-    elif x == 2:
-      return True
-    elif x%2 == 0:
-      return False
-        
-    i = 3
-    while i*i <= x:
-        if x%i == 0:
-            return False
-        i += 2
+    i += 2
     return True
     
 num = isPrime(int(input()))  
